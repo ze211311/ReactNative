87bca97fa50fc3bcfadbefa0907d82aa2855dcd9
ze211311 <ze.narongkorn_st@tni.ac.th>
2023-06-24 12:19:01 +0700

    Catjs

--- components/Cat.js
+++ components/Cat.js
@@ -1,10 +1,16 @@
 import { View, Text } from 'react-native'
 import React from 'react'
 
+const getFullName = (Fn, Sn, Tn) => {
+    return Fn + ' ' + Sn + ' ' + Tn;
+}
+
 const Cat = () => {
   return (
     <View>
-      <Text>Cat</Text>
+      <Text>
+        Hello, I am {getFullName('Lily', 'Louis', 'Luca')}!
+      </Text>
     </View>
   )
 }
