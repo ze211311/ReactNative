51c8c5809bc531dc8d3ebd468bb45117f310c505
Ze211311 <ze.narongkorn_st@tni.ac.th>
2023-08-26 11:49:44 +0700

    Second Commit

--- App.js
+++ App.js
@@ -2,18 +2,20 @@ import { StatusBar } from 'expo-status-bar';
 import { StyleSheet, Text, View } from 'react-native';
 //import Counter from './components/counter.js';
 //import  MyInput  from './components/MyInput.js';
-import IncrementCounter from './components/IncrementCounter'
+/* import IncrementCounter from './components/IncrementCounter'
 import Form from './components/Form'
 import Login from './components/Login'
 import TP from './javascriptFile/TouchablePractice'
 import Example_useEffect from './javascriptFile/Example_useEffect';
 import UseEffectFlatList from './javascriptFile/useEffectFlatList';
 import FlatlistAPI from './javascriptFile/FlatlistAPI';
-import News from './javascriptFile/News';
+import News from './javascriptFile/News'; */
+import ProductScreen from './javascriptFile/ProductScreen';
+
 const App = () => {
   return (
-    <View style={styles.container}>
-      <News/>
+    <View style={{flex:1}}>
+      <ProductScreen/>
     </View>
   );
 }
