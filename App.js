b85fe37f215ed67a75f7cecddd8dc0522054f829
Ze211311 <ze.narongkorn_st@tni.ac.th>
2023-09-02 09:23:36 +0700

    new install

--- App.js
+++ App.js
@@ -2,14 +2,14 @@ import { StatusBar } from 'expo-status-bar';
 import { StyleSheet, Text, View } from 'react-native';
 //import Counter from './components/counter.js';
 //import  MyInput  from './components/MyInput.js';
-/* import IncrementCounter from './components/IncrementCounter'
+import IncrementCounter from './components/IncrementCounter'
 import Form from './components/Form'
 import Login from './components/Login'
 import TP from './javascriptFile/TouchablePractice'
 import Example_useEffect from './javascriptFile/Example_useEffect';
 import UseEffectFlatList from './javascriptFile/useEffectFlatList';
 import FlatlistAPI from './javascriptFile/FlatlistAPI';
-import News from './javascriptFile/News'; */
+import News from './javascriptFile/News';
 import ProductScreen from './javascriptFile/ProductScreen';
 
 const App = () => {
@@ -24,6 +24,6 @@ const styles = StyleSheet.create({
   container:{
       flex:1,
       justifyContent: 'center',
-        alignItems: 'Center'
+      alignItems: 'Center'
   }
 })
